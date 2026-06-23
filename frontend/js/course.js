5e2449c5170faae10066ab6e895b71943bb144b9
Win Htein Oo <winhteinoo.26@gmail.com>
2026-06-23 01:55:30 -0400

    Format updateGrade function for consistency

--- frontend/js/course.js
+++ frontend/js/course.js
@@ -1,4 +1,5 @@
-function updateGrade(){ 
-  document.getElementById("grade").innerHTML = "90%"; 
-  document.getElementById("bar").style.width = "90%"; }
+function updateGrade() {
+    document.getElementById("grade").innerHTML = "90%";
+    document.getElementById("bar").style.width = "90%";
+}
 
