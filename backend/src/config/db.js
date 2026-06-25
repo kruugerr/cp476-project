72c65d7ae4e75b6ee5d1670931a09e34b0d87feb
fanggodzzz <phan7085@mylaurier.ca>
2026-06-25 10:50:42 -0400

    Change MySQL to PostgreSQL

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,17 +1,18 @@
 import dotenv from "dotenv";
-import mysql from "mysql2/promise";
+import { Pool } from "pg";
 
 dotenv.config();
 
-const pool = mysql.createPool({
+const pool = new Pool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
 
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
+    max: 10,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
 });
 
 let dbExists = false;
@@ -19,8 +20,8 @@ let dbExists = false;
 // Function to check if the database exists, if not runs without database
 export const initDB = async () => {
     try {
-        const conn = await pool.getConnection();
-        await conn.ping();
+        const conn = await pool.connect();
+        await client.query("SELECT 1");
         conn.release();
         dbExists = true;
     } catch (err) {
